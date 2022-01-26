deea0f211cdfc6327214060373758c3d8d6f0bb7
soyidwahyudarmawan <soyid.wahyu@bfi.co.id>
2022-01-26 15:18:48 +0700

    branch darmawan

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hai Wahyu");
         System.out.println("Hai Soyid");
+        System.out.println("Hai Wahyu");
+        System.out.println("Hai Darmawan");
+        System.out.println("Kamu Ganteng Deh");
     }
 }
